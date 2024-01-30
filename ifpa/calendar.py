feaf3bc279b33294ff50dcd4f382332d9fc9adea
tlastowka <tlastowka@gmail.com>
2024-01-30 14:35:25 -0500

    silly

--- ifpa/calendar.py
+++ ifpa/calendar.py
@@ -13,7 +13,7 @@ def calendar_active(api_key, country=None, request_params={}, *args, **kwargs):
     """
 
     url = f"""calendar/active"""
-    return __get(api_key, url, params=request_params, *args, **kwargs)
+    return _get(api_key, url, params=request_params, *args, **kwargs)
 
 
 def calendar_history(api_key, country=None, request_params={}, *args, **kwargs):
@@ -24,7 +24,7 @@ def calendar_history(api_key, country=None, request_params={}, *args, **kwargs):
     """
 
     url = f"""calendar/history"""
-    return __get(api_key, url, params=request_params, *args, **kwargs)
+    return _get(api_key, url, params=request_params, *args, **kwargs)
 
 
 def calendar(api_key, calendar_id, request_params={}, *args, **kwargs):
@@ -35,7 +35,7 @@ def calendar(api_key, calendar_id, request_params={}, *args, **kwargs):
     """
 
     url = f"""calendar/{calendar_id}"""
-    return __get(api_key, url, params=request_params, *args, **kwargs)
+    return _get(api_key, url, params=request_params, *args, **kwargs)
 
 
 def calendar_search(api_key, address=None, m=None, k=None, request_params={}, *args, **kwargs):
@@ -46,4 +46,4 @@ def calendar_search(api_key, address=None, m=None, k=None, request_params={}, *a
     """
 
     url = f"""calendar/search"""
-    return __get(api_key, url, params=request_params, *args, **kwargs)
+    return _get(api_key, url, params=request_params, *args, **kwargs)
